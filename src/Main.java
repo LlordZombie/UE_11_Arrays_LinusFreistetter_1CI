9f14ca7cba3cb66ca69f81c82cf16cfe904a12a3
LlordZombie <linus.freistetter@hotmail.com>
2023-02-20 15:07:49 +0100

    fertig juhu

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,11 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        int[] a = {1, 2, 3, 4, 5, 6, 7, 8};
-        System.out.println("contains(a,5) = " + contains(a, 9));
+        String[] a = {"Hallo", "ich", "bin", "hier", "!"};
+        System.out.println("contains(a,bin) = " + contains(a, "bin"));
         System.out.println("getLottoTipp() = " + Arrays.toString(getLottoTipp()));
         System.out.println("lottoTippToString(getLottoTipp()) = " + lottoTippToString(getLottoTipp()));
+        System.out.println("getNLottoTips(5) = " + Arrays.toString(getNLottoTips(5)));
     }
 
     public static int random(int min, int max) {
@@ -37,9 +38,62 @@ public class Main {
     }
 
     public static String lottoTippToString(int[] lottoTipp) {
-        String rString = "";
+        String rString;
         Arrays.sort(lottoTipp);
         rString = String.format("%02d %02d %02d %02d %02d %02d %n", lottoTipp[0], lottoTipp[1], lottoTipp[2], lottoTipp[3], lottoTipp[4], lottoTipp[5]);
         return rString;
     }
+
+
+    public static boolean contains(String[] a, String search) {
+        for (String j : a) {
+            if (j.equals(search)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String[] getNLottoTips(int n) {
+        String[] allLottoTips = new String[n];
+        for (int i = 0; i < n; i++) {
+            allLottoTips[i] = "";
+        }
+        int[] singleLottoTip = new int[6];
+        int possibleNum;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 6; j++) {
+                possibleNum = random(1, 45);
+                if (contains(singleLottoTip, possibleNum)) {
+                    j--;
+                } else {
+                    singleLottoTip[j] = possibleNum;
+                }
+            }
+            Arrays.sort(singleLottoTip);
+            if (!contains(allLottoTips, Arrays.toString(singleLottoTip))) {
+                allLottoTips[i] = Arrays.toString(singleLottoTip);
+            } else {
+                i--;
+            }
+        }
+        return allLottoTips;
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
